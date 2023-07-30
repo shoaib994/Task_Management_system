241ebf3323b03582a39c7ed0ce56062c38b6e504
Shoaib Ahmed <shabiraja994@gmail.com>
2023-07-30 16:20:17 +0500

    change backend route

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducer/task";
 
 
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://task-backend-pi.vercel.app/api/v1";
 
 const store = configureStore({
   reducer: {
